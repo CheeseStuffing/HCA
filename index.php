50221c97e5b4a64ad222a75ca8e975debb2aefbe
CheeseStuffing <42998889+CheeseStuffing@users.noreply.github.com>
2019-12-16 02:40:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="da">
+<?php include('header.php'); ?>
+<body id="frontpage">
+	<?php include('navigation.php'); ?>
+
+	<main>
+		<div class="intro fadeInUp hca"><img src="images/hca.png"></div>
+		<div class="intro fadeInUp kejseren"><img src="images/kejseren.png"></div>
+		<div class="intro fadeInUp klodshans"><img src="images/klodshans.png"></div>
+		<div class="intro fadeInUp havfrue"><img src="images/mermaid.png"></div>
+		<div class="intro fadeInUp nattergal"><img src="images/nattergal.png"></div>
+		<div class="intro fadeInUp pigenmed"><img src="images/pigenmed.png"></div>
+		<div class="intro fadeInUp prinsessen"><img src="images/prinsessen.png"></div>
+		<div class="intro fadeInUp snedronning"><img src="images/snedronning.png"></div>
+		<div class="intro fadeInUp tinsoldat"><img src="images/tinsoldat.png"></div>
+		<div class="intro fadeInUp uglyduckling"><img src="images/uglyduckling.png"></div>
+		<div class="introduction">
+			<h1>HCA Paraden</h1>
+			<p class="intro-p1">Danmarks nationalt og internationalt prisbelønnede H.C. Andersen Paraden er <span>kunst</span>, <span>kultur</span> og <span>folklore</span> på én gang.</p>
+			<p class="intro-p2">Eventyrlig underholdning for store og små – og både danske og udenlandske gæster.</p>
+			<button><a href="pages.php?page=booking" alt="Booking">Book os</a></button>
+		</div>
+	</main>
+
+	<footer class="regFooter frontpageFooter">
+		<?php include('footer.php'); ?>
+	</footer>
+</body>
+</html>
