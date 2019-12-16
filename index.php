fcc3d71ec2b39cd799975ccc37ab4e1b82fd96a6
CheeseStuffing <42998889+CheeseStuffing@users.noreply.github.com>
2019-12-16 08:53:39 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="da">
+<?php include('header.php'); ?>
+<body id="frontpage">
+	<?php include('navigation.php'); ?>
+
+	<main>
+		<div class="intro fadeInUp hca"><img src="images/hca.png" alt="H. C. Andersen"></div>
+		<div class="intro fadeInUp kejseren"><img src="images/kejseren.png" alt="Kejserens Nye Klæder"></div>
+		<div class="intro fadeInUp klodshans"><img src="images/klodshans.png" alt="Klods Hans"></div>
+		<div class="intro fadeInUp havfrue"><img src="images/mermaid.png" alt="Den Lille Havfrue"></div>
+		<div class="intro fadeInUp nattergal"><img src="images/nattergal.png" alt="Nattergalen"></div>
+		<div class="intro fadeInUp pigenmed"><img src="images/pigenmed.png" alt="Pigen med svovlstikkerne"></div>
+		<div class="intro fadeInUp prinsessen"><img src="images/prinsessen.png" alt="Prinsessen på ærten"></div>
+		<div class="intro fadeInUp snedronning"><img src="images/snedronning.png" alt="Snedronningen"></div>
+		<div class="intro fadeInUp tinsoldat"><img src="images/tinsoldat.png" alt="Fyrtøjet"></div>
+		<div class="intro fadeInUp uglyduckling"><img src="images/uglyduckling.png" alt="Den Grimme Ælling"></div>
+		<div class="introduction">
+			<h1>HCA Paraden</h1>
+			<p class="intro-p1">Danmarks nationalt og internationalt prisbelønnede H.C. Andersen Paraden er <span>kunst</span>, <span>kultur</span> og <span>folklore</span> på én gang.</p>
+			<p class="intro-p2">Eventyrlig underholdning for store og små – og både danske og udenlandske gæster.</p>
+			<button><a href="pages.php?page=booking">Book os</a></button>
+		</div>
+	</main>
+
+	<footer class="regFooter frontpageFooter">
+		<?php include('footer.php'); ?>
+	</footer>
+</body>
+</html>
